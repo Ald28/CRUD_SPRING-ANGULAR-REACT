75e70e15492e09350f4f3786732f1b452f8f7eb3
Ald28 <aldopichihua123@gmail.com>
2024-11-28 10:00:17 -0500

    primer commit

---
+++ src/views/Inicio.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Inicio = () => {
+    return (
+        <div className="container">
+            <h1>Inicio</h1>
+            <p>Bienvenido a la página principal.</p>
+        </div>
+    );
+};
+
+export default Inicio;
